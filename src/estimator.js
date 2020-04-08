d37be889240ab58bb813dbb0e00b62c424d91804
Emmanuel <ezeemmanuel2010@gmail.com>
2020-04-08 07:02:35 +0100

    feature(estimator):build impact estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,35 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const currentlyInfectedImpact = data.reportedCases * 10;
+  const currentlyInfectedSevereImpact = data.reportedCases * 50;
+
+  const infectionsRequestByTimeImpact = currentlyInfectedImpact * 512;
+  const infectionsRequestByTimeSevere = currentlyInfectedSevereImpact * 512;
+
+  return {
+    data,
+    impact: {
+      currentlyInfected: currentlyInfectedImpact,
+      infectionsRequestByTime: infectionsRequestByTimeImpact
+    },
+    severeImpact: {
+      currentlyInfected: currentlyInfectedSevereImpact,
+      infectionsRequestByTime: infectionsRequestByTimeSevere
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
+
+covid19ImpactEstimator({
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+});
