984f6bfe4c476afea8430d24dd5f3e308161059f
Emmanuel <ezeemmanuel2010@gmail.com>
2020-04-10 09:31:06 +0100

    bug(population):fix avg daily income of population

--- src/utility.js
+++ src/utility.js
@@ -5,22 +5,26 @@ const utility = {
   },
 
   calculateDollarsInFlight(data, infectionsImpact, infectionsSevere) {
-    const { region, timeToElapse } = data;
+    const { region, timeToElapse, population } = data;
     const avgDailyInc = region.avgDailyIncomeInUSD;
     const avgDailyIncPop = region.avgDailyIncomePopulation;
 
+    // total income iN USD
+    const totalIncomeOfThePopulation =
+      population * avgDailyInc * avgDailyIncPop * timeToElapse;
+
+    const totalIncomeOfTheImpact =
+      infectionsImpact * avgDailyInc * avgDailyIncPop * timeToElapse;
+
+    const totalIncomeOfTheSevere =
+      infectionsSevere * avgDailyInc * avgDailyIncPop * timeToElapse;
+
     const dollarsInFlightImpact = (
-      infectionsImpact *
-      avgDailyInc *
-      avgDailyIncPop *
-      timeToElapse
+      totalIncomeOfThePopulation - totalIncomeOfTheImpact
     ).toFixed(2);
 
     const dollarsInFlightSevere = (
-      infectionsSevere *
-      avgDailyInc *
-      avgDailyIncPop *
-      timeToElapse
+      totalIncomeOfThePopulation - totalIncomeOfTheSevere
     ).toFixed(2);
 
     return { dollarsInFlightImpact, dollarsInFlightSevere };
