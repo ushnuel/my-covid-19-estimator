2ef4c4404412214ed77b0f56f7ff3072c256abf7
Emmanuel <ezeemmanuel2010@gmail.com>
2020-04-14 16:29:47 +0100

    bug(output):remove data from output

--- src/estimator_controller.js
+++ src/estimator_controller.js
@@ -11,7 +11,7 @@ const estimateValue = (data) => CovidEstimator(data);
 const estimatorController = {
   estimate(req, res, next) {
     const estimatedValues = estimateValue(req.body);
-    res.status(200).json({ data: estimatedValues });
+    res.status(200).json({ ...estimatedValues });
     return estimatedValues;
   },
 
